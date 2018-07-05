a1b66db3ce7d208bfd6828ac2e7409ef5e69e2d2
Sanghamitra <sanghamitrav26@gmail.com>
2018-07-05 22:25:56 +0530

    modified

--- task_1.sh
+++ task_1.sh
@@ -6,16 +6,19 @@ echo "Hod" | xargs sudo useradd -m -s /bin/bash -c "Hod"
 sudo passwd Hod
 #sudo chsh -s /bin/bash Hod
 sudo -u Hod mkdir /home/Hod/My_Folder
-sudo chown Hod: /home/Hod
+#sudo chown Hod: /home/Hod
+sudo setfacl -m "u:Hod:u+w" /home/Hod
 i=1
 while [ $i -le 2 ]
 do
   echo "Prof${i}" | xargs sudo useradd -m -s /bin/bash -c "Prof${i}"
   sudo passwd Prof${i}
   sudo -u Prof${i} mkdir /home/Prof${i}/Teaching_Material
-  sudo chmod u+w /home/Hod /home/Prof${i}
-  sudo chown Prof${i}: /home/Prof${i}
-  sudo chmod u+w /home/Prof${i}
+  #sudo chmod u+w /home/Hod /home/Prof${i}
+  #sudo chown Prof${i}: /home/Prof${i}
+  #sudo chmod u+w /home/Prof${i}
+  sudo setfacl -m "u:Hod:u+w" /home/Prof${i}
+  sudo setfacl -m "u:Prof${i}:u+w" /home/Prof${i}
   x=1
   while [ $x -le 50 ]
   do
@@ -33,14 +36,17 @@ do
   sudo -u Student${i_1} mkdir /home/Student${i_1}/HomeWork/{Prof_1 Work, Prof_2 Work}
   sudo shuf -zn8 -e /home/Prof1/*.txt | xargs -0 cp -vt /home/Student${i_1}/HomeWork/Prof_1 Work
   sudo shuf -zn8 -e /home/Prof2/*.txt | xargs -0 cp -vt /home/Student${i_1}/HomeWork/Prof_2 Work
-  sudo chmod u+w /home/Hod /home/Student${i_1}
+  #sudo chmod u+w /home/Hod /home/Student${i_1}
+  sudo setfacl -m "u:Hod:u+w" /home/Student${i_1}
   while [ $i -le 2 ]
   do
-    sudo chown Prof${i}: /home/Student${i_1}
+    #sudo chown Prof${i}: /home/Student${i_1}
+    sudo setfacl -m "u:Prof${i}:u+w" /home/Student${i_1}
   ((i++))
   done
-  sudo chown Student${i_1}: /home/Student${i_1}
-  sudo chmod u+w /home/Student${i_1}   
+  #sudo chown Student${i_1}: /home/Student${i_1}
+  #sudo chmod u+w /home/Student${i_1}
+  sudo setfacl -m "u:Student${i_1}:u+w" /home/Student${i_1}   
   ((i_1++))
 done
 #sudo chown Hod: /home/Hod
